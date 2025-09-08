802409a79d0b8d5e43d2365e52f67169b82772c6
Alan Covarrubias <alsacovarrubiaspe@ittepic.edu.mx>
2025-09-08 12:13:41 -0700

    Elementos y funciones removidas.
    Se removieron los elementos y funciones que a primera vista no seran contempladas para el back-end del proyecto.

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,13 +3,7 @@ import config from "../config/config.js";
 
 import authRoutes from "./auth.routes.js";
 
-import bdp_busquedasRoutes from "./bdp_busquedas.routes.js";
-
 import bdp_parametrosRoutes from "./bdp_parametros.routes.js";
-
-import bdp_reg_victimasRoutes from "./bdp_reg_victimas.routes.js";
-
-import bdp_prereg_victimasRoutes from "./bdp_prereg_victimas.routes.js";
 
 import bdp_nacionalidadesRoutes from "./bdp_cat_nacionalidades.routes.js";
 
@@ -29,13 +23,7 @@ const routerAPI = (app) => {
 
   router.use("/auth", authRoutes);
 
-  router.use("/busquedas", bdp_busquedasRoutes);
-
   router.use("/parametros", bdp_parametrosRoutes);
-
-  router.use("/regvictimas", bdp_reg_victimasRoutes);
-
-  router.use("/preregvictimas", bdp_prereg_victimasRoutes);
 
   router.use("/nacionalidades", bdp_nacionalidadesRoutes);
 
